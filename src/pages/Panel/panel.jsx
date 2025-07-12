04ac66476ce4de6acb05d8bf66460e6143b4f0c8
mohammadhamtaco <167424196+mohammadhamtaco@users.noreply.github.com>
2025-07-12 16:17:41 +0330

    Admin panel data entry grid created for calling api and fetching data--not tested

--- src/pages/Panel/panel.jsx
+++ src/pages/Panel/panel.jsx
@@ -1,14 +1,56 @@
-import React, { } from "react";
+import React, { useEffect, useState } from "react";
 import AuthModal from "../../components/authModal/authModal";
+import axios from "axios";
 import "./panel.css";
 
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
 function Panel() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await API.get("/panel");
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setData([]);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="panel">
-      <AuthModal />
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis corporis reiciendis sint repudiandae, facere corrupti odit dignissimos placeat alias iure nihil eveniet ratione voluptate doloribus harum autem suscipit, sequi ipsa!
+      {/* <AuthModal /> */}
+      <div className="container my-5">
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">ردیف</th>
+              <th scope="col">نام و نام خانوادگی</th>
+              <th scope="col">کد ملی</th>
+              <th scope="col">موبایل</th>
+              <th scope="col">وضعیت</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <th scope="row">{index + 1}</th>
+                <td>{item.name}</td>
+                <td>{item.nationalId}</td>
+                <td>{item.mobile}</td>
+                <td>{item.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 export default Panel;
